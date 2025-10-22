a7619da28d68f1095ac008a2c73ce5b46688fb0b
ChermSlerm <silas412294@gmail.com>
2025-10-22 16:36:52 -0400

    Installed and configured eslint, prettier, and react-compiler

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  pluginReact.configs.flat.recommended, reactHooks.configs.flat.recommended,
+]);
